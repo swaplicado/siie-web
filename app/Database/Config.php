d37f11199d57f05a7c25b605689ae770de7f95ab
eocarmona <ecarmona.swaplicado@gmail.com>
2018-04-19 16:41:02 -0500

    connection

--- app/Database/Config.php
+++ app/Database/Config.php
@@ -16,10 +16,10 @@
         $lDataBases = array();
 
         $i = 0;
-        // $lDataBases[$i++] = 'siie_sap';
-        // $lDataBases[$i++] = 'siie_gsc';
-        $lDataBases[$i++] = 'siie_saporis';
-        $lDataBases[$i++] = 'siie_gs';
+        $lDataBases[$i++] = 'siie_sap';
+        $lDataBases[$i++] = 'siie_gsc';
+        // $lDataBases[$i++] = 'siie_saporis';
+        // $lDataBases[$i++] = 'siie_gs';
 
         return $lDataBases;
     }
