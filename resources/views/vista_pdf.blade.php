51cd596df6019c9a8d87e1dae5f20c77a2af800a
eocarmona <ecarmona.swaplicado@gmail.com>
2018-04-27 11:50:36 -0500

    lot label

--- resources/views/vista_pdf.blade.php
+++ resources/views/vista_pdf.blade.php
@@ -1,8 +1,11 @@
 
-<div style="font-size:9px; margin-top:0px;">
+<div style="font-size:66%; margin:-2px; padding:-20px; bottom:-20px; margin-right:-20px">
   {{$data->lot}}-{{$data->item->name}}-{{$data->unit->name}}
   <br>
-  <img src="data:image/png;base64,{{DNS1D::getBarcodePNG($barcode, 'C128',1,33)}}" alt="barcode" height="60%" width="60%"/>
-  <br>
-  {{$barcode}} - {{session('company')->name}}
+  <center>
+    <img src="data:image/png;base64,{{DNS1D::getBarcodePNG($barcode, 'C128',1,33)}}" alt="barcode" height="60%" width="60%"/>
+  </center>
+  <center>
+    {{$barcode}} - {{session('company')->name}}
+  </center>
 </div>
